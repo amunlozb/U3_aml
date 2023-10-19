cf8e15770d2083993eb56120ee6ae2d676b592c6
amunlozb <amunlozb@g.educaand.es>
2023-10-19 14:11:01 +0200

    asd

--- src/main/java/com/example/u3_aml/controller/EstudianteControlador.java
+++ src/main/java/com/example/u3_aml/controller/EstudianteControlador.java
@@ -2,29 +2,35 @@ package com.example.u3_aml.controller;
 
 import com.example.u3_aml.data.DataEstudiantes;
 import com.example.u3_aml.model.Estudiante;
+import com.example.u3_aml.service.EstudianteServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class EstudianteControlador {
+    EstudianteServicio servicio = new EstudianteServicio();
 
-    @GetMapping("/listarEstudiantes")
-    public List<Estudiante> listarEstudiantes(Model model) {
-        DataEstudiantes datos = new DataEstudiantes();
-        model.addAttribute("listaEstudiantes", datos.getLista());
-
-        return (datos.getLista());
+    @GetMapping("/")
+    public String listarEstudiantes(Model model) {
+        List<Estudiante> estudiantes = servicio.obtenerListaEstudiantes();
+        model.addAttribute("listaEstudiantes", estudiantes);
+        model.addAttribute("nombre", "");
+        return "templateEstudiantes";
     }
 
     @GetMapping("/buscarEstudiantes")
-    public List<Estudiante> buscarEstudiantes() {
-        DataEstudiantes datos = new DataEstudiantes();
-
-        return (datos.getLista());
+    public String buscarEstudiantes(@RequestParam("nombre") String nombre, Model model) {
+        List<Estudiante> listaFiltrada = servicio.filtrarEstudiantes(servicio.obtenerListaEstudiantes(), nombre);
+        model.addAttribute("listaEstudiantes", listaFiltrada);
+         model.addAttribute("nombre", nombre);
+        return "templateEstudiantes";
     }
 
     @GetMapping("/filtrarEstudiantes")
--- src/main/java/com/example/u3_aml/service/EstudianteServicio.java
+++ src/main/java/com/example/u3_aml/service/EstudianteServicio.java
@@ -2,18 +2,29 @@ package com.example.u3_aml.service;
 
 import com.example.u3_aml.data.DataEstudiantes;
 import com.example.u3_aml.model.Estudiante;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static java.util.Arrays.stream;
+
+@Service
 public class EstudianteServicio {
     DataEstudiantes datos = new DataEstudiantes();
 
-    public List<Estudiante> listarEstudiantes() {
+    /**
+    *  Recupera la lista completa de estudiantes
+     * @return Una lista de objetos de estudiante
+    * */
+    public List<Estudiante> obtenerListaEstudiantes() {
         List<Estudiante> lista = datos.getLista();
-
-        lista.add(new Estudiante());
-
         return lista;
     }
 
+    public List<Estudiante> filtrarEstudiantes(List<Estudiante> lista, String nombre) {
+        return lista.stream()
+                .filter(e -> e.getNombre().contains(nombre))
+                .collect(Collectors.toList());
+    }
 }
